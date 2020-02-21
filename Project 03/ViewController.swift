627594e160f28788e65cc87ccb4ec0188f35ff33
Aleksandr Kurdiukov <runoleonard@gmail.com>
2020-02-21 12:46:31 +0100

    6.3

--- Project 03/ViewController.swift
+++ Project 03/ViewController.swift
@@ -9,12 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var firstNumber: UITextField!
+    
+    @IBOutlet weak var sign: UITextField!
+    
+    @IBOutlet weak var secondNumber: UITextField!
+    
+    @IBAction func apply(_ sender: Any) {
+        
+        let convN1 = Int(firstNumber.text!)
+        let convN2 = Int(secondNumber.text!)
+        let error = "Некорректные данные"
+        var answer = error
+        
+        if convN1 == nil || convN2 == nil {
+            label.text! = error
+        } else {
+            
+            switch sign.text! {
+            case "+":
+                answer = "\(Double(convN1!) + Double(convN2!))"
+            case "-":
+                answer = "\(Double(convN1!) - Double(convN2!))"
+            case "*":
+                answer = "\(Double(convN1!) * Double(convN2!))"
+            case "/":
+                answer = "\(Double(convN1!) / Double(convN2!))"
+            default:
+                answer = error
+            }
+            
+            label.text! = answer
+        }
     }
-
-
 }
 
